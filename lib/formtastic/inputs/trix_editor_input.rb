1d7606143eedb6c6a751eed353ca4c87d3d4e8dc
Christian <github@christian-warnecke.de>
2018-09-20 16:43:45 +0200

    fix: editor doesn't get populated with stored content

--- lib/formtastic/inputs/trix_editor_input.rb
+++ lib/formtastic/inputs/trix_editor_input.rb
@@ -9,7 +9,7 @@ class TrixEditorInput < Formtastic::Inputs::StringInput
       editor_tag = template.content_tag('trix-editor', '', editor_tag_params)
       hidden_field = builder.hidden_field(method, input_html_options)
 
-      editor = template.content_tag('div', editor_tag + hidden_field, class: 'trix-editor-wrapper')
+      editor = template.content_tag('div', hidden_field + editor_tag, class: 'trix-editor-wrapper')
 
       label_html + editor
     end
