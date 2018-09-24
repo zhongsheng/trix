1b31871fb23ad670af12c2fa814869c964d1432f
James Miller <bensie@gmail.com>
2018-09-24 10:26:03 -0700

    Fix simple_form displaying stored data
    Follow-up to #9, but fixes the same issue for simple form

--- lib/trix/simple_form/trix_editor_input.rb
+++ lib/trix/simple_form/trix_editor_input.rb
@@ -8,7 +8,7 @@ module Trix
         editor_tag = template.content_tag('trix-editor', '', editor_options)
         hidden_field = @builder.hidden_field(attribute_name, input_html_options)
 
-        template.content_tag('div', editor_tag + hidden_field, class: 'trix-editor-wrapper')
+        template.content_tag('div', hidden_field + editor_tag, class: 'trix-editor-wrapper')
       end
     end
   end
